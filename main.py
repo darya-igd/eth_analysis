55c59d78e647e788278c6676cea4d768fdd31f8d
daria igdalov <igdalovdarya@gmail.com>
2026-02-14 12:44:39 +0200

    Project setup complete and Excel preview verified

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from src.data_loader import load_transactions
 
-df= load_transactions('data/transactions.xlsx')
+df= load_transactions("data/transactions.xlsx")
+
+print("first 5 rows of the transactions data:")
 print(df.head())
+
+print("\nColumns:")
+print(df.columns)
+
+print("\nInfo:")
+print(df.info())
 
